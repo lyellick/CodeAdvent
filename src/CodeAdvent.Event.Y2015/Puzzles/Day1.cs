3f22ad6fa44c608e835038660d1fe34268c5a09f
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-20 14:21:50 -0600

    event 2015 setup

---
+++ src/CodeAdvent.Event.Y2015/Puzzles/Day1.cs
@@ -0,0 +1,22 @@
+namespace CodeAdvent.Event.Y2015.Puzzles
+{
+    public class Day1
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.Pass();
+        }
+    }
+}
