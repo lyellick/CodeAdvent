ce204734734c9edad8783728edef321320936f88
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-21 16:32:41 -0600

    day 7 creation

---
+++ src/CodeAdvent.Event.Y2015/Puzzles/Day7.cs
@@ -0,0 +1,28 @@
+namespace CodeAdvent.Event.Y2015.Puzzles
+{
+    /// <summary>
+    /// Day 7: Some Assembly Required
+    /// </summary>
+    public class Day7
+    {
+        private string _input;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _input = await CodeAdventData.GetData(2015, 7);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.Pass();
+        }
+    }
+}
