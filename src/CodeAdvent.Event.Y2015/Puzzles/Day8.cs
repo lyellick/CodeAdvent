e9cab73f6fc3d115f5b8722d18bca0cb656a5841
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-22 13:30:32 -0600

    day 8 creation

---
+++ src/CodeAdvent.Event.Y2015/Puzzles/Day8.cs
@@ -0,0 +1,28 @@
+namespace CodeAdvent.Event.Y2015.Puzzles
+{
+    /// <summary>
+    /// Day 8: Matchsticks
+    /// </summary>
+    public class Day8
+    {
+        private string _input;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _input = await CodeAdventData.GetData(2015, 8);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.Pass();
+        }
+    }
+}
