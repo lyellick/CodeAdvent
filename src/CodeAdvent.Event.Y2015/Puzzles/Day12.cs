9e3744eec3337cddb7b814566d98ac8ecf25a0f6
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-23 17:12:34 -0600

    complate day 12 part 1

---
+++ src/CodeAdvent.Event.Y2015/Puzzles/Day12.cs
@@ -0,0 +1,38 @@
+using System.Text.RegularExpressions;
+
+namespace CodeAdvent.Event.Y2015.Puzzles
+{
+    /// <summary>
+    /// Day 12: JSAbacusFramework.io
+    /// </summary>
+    public class Day12
+    {
+        private string _input;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _input = await CodeAdventData.GetData(2015, 12);
+
+            Assert.That(_input, Is.Not.Null.Or.Empty);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Regex integers = new(@"-?\d+");
+
+            var matches = integers.Matches(_input);
+
+            int sum = matches.Sum(match => int.Parse(match.Value));
+
+            Assert.That(sum, Is.EqualTo(191164));
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.Pass();
+        }
+    }
+}
