ba4b91ed18b37506f1a74a59a002cb47b01fca3a
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-23 21:33:11 -0600

    continuing day 13 part 1

--- src/CodeAdvent.Event.Y2015/Puzzles/Day13.cs
+++ src/CodeAdvent.Event.Y2015/Puzzles/Day13.cs
@@ -1,3 +1,6 @@
+using System.Globalization;
+using System.Text.RegularExpressions;
+
 namespace CodeAdvent.Event.Y2015.Puzzles
 {
     /// <summary>
@@ -18,6 +21,8 @@ namespace CodeAdvent.Event.Y2015.Puzzles
         [Test]
         public void Part1()
         {
+            var plan = ProcessOptimalSeatingArrangement(_input);
+
             Assert.Pass();
         }
 
@@ -26,5 +31,52 @@ namespace CodeAdvent.Event.Y2015.Puzzles
         {
             Assert.Pass();
         }
+
+        private object ProcessOptimalSeatingArrangement(string input)
+        {
+            var requests = MapIdealNeighbors(input);
+
+            var guests = requests.Select(request => request.guest).ToHashSet().ToList();
+
+            var guestPermutations = guests.GeneratePermutations();
+
+            var datasets = GenerateDatasets(guestPermutations);
+
+            return new { };
+        }
+
+        private IEnumerable<(IList<string> permutation, IList<string[]> splits, int units)> GenerateDatasets(IEnumerable<IList<string>> permutations)
+        {
+            foreach (var permutation in permutations)
+            {
+                List<string[]> splits = new();
+
+                for (int i = 0; i < permutation.Count; i++)
+                    if (i < permutation.Count - 1)
+                        splits.Add(new string[] { permutation[i], permutation[i + 1] });
+
+                // Catch the loop around so the last guest can be matched up with the first guest in the permuation.
+                splits.Add(new string[] { permutation[^1], permutation[0] });
+
+                yield return (permutation, splits, 0);
+            }
+        }
+
+        private IEnumerable<(string guest, Outcome outcome, int units, string neighbor)> MapIdealNeighbors(string input)
+        {
+            Regex pattern = new(@"(.*) would (.*) (.*) happiness units by sitting next to (.*).");
+
+            using var reader = new StringReader(input);
+
+            for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
+            {
+                var found = pattern.Match(line);
+
+                if (Enum.TryParse(CultureInfo.CurrentCulture.TextInfo.ToTitleCase(found.Groups[2].Value), out Outcome outcome) && int.TryParse(found.Groups[3].Value, out int units))
+                    yield return (found.Groups[1].Value, outcome, units, found.Groups[4].Value);
+            }
+        }
+
+        private enum Outcome { Gain, Lose, None }
     }
 }
