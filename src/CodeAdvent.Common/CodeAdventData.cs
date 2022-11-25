b19e0ecfe5ccd09a164209ce613176dd479ffae6
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-25 14:51:13 -0600

    added helper method to map input data

--- src/CodeAdvent.Common/CodeAdventData.cs
+++ src/CodeAdvent.Common/CodeAdventData.cs
@@ -1,4 +1,5 @@
 ﻿using RestSharp;
+using System.Text.RegularExpressions;
 
 namespace CodeAdvent.Common
 {
@@ -45,6 +46,16 @@ namespace CodeAdvent.Common
             }
 
             return null;
+        }
+
+        public static IEnumerable<T> MapData<T>(string input, string pattern, Func<Match, T> action)
+        {
+            Regex find = new(pattern);
+
+            using var reader = new StringReader(input);
+
+            for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
+                yield return action(find.Match(line));
         }
 
         private static bool TryGetEnvironmentVariable(string key, out string value)
