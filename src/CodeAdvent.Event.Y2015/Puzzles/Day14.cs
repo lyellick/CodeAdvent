4a6a347d3e84a1cd958bf608e0889d4488e8819d
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-24 21:59:02 -0600

    day 14 part 1 continued...

--- src/CodeAdvent.Event.Y2015/Puzzles/Day14.cs
+++ src/CodeAdvent.Event.Y2015/Puzzles/Day14.cs
@@ -1,3 +1,8 @@
+using System.Diagnostics;
+using System.Globalization;
+using System.Reflection.PortableExecutable;
+using System.Text.RegularExpressions;
+
 namespace CodeAdvent.Event.Y2015.Puzzles
 {
     /// <summary>
@@ -10,7 +15,7 @@ namespace CodeAdvent.Event.Y2015.Puzzles
         [SetUp]
         public async Task Setup()
         {
-            _input = await CodeAdventData.GetData(0, 0);
+            _input = await CodeAdventData.GetData(2015, 14);
 
             Assert.That(_input, Is.Not.Null.Or.Empty);
         }
@@ -18,6 +23,8 @@ namespace CodeAdvent.Event.Y2015.Puzzles
         [Test]
         public void Part1()
         {
+            var results = ProcessReindeerSimulation(_input, 2503).OrderByDescending(simulation => simulation.distance);
+
             Assert.Pass();
         }
 
@@ -25,6 +32,37 @@ namespace CodeAdvent.Event.Y2015.Puzzles
         public void Part2()
         {
             Assert.Pass();
+        }
+
+        private (string reindeer, int speed, int duration, int rest, int distance, bool fly)[] ProcessReindeerSimulation(string input, int seconds)
+        {
+            var reindeer = MapReindeerStats(input).ToArray();
+
+            reindeer = new (string reindeer, int speed, int duration, int rest, int distance, bool fly)[] { ("Comet", 14, 10, 127, 0, true), ("Dancer", 16, 11, 162, 0, true) };
+
+            seconds = 1000;
+
+            for (int i = 0; i < reindeer.Length; i++)
+            {
+                // TODO: Magic calculations...
+            }
+
+            return reindeer;
+        }
+
+        private IEnumerable<(string reindeer, int speed, int duration, int rest, int distance, bool fly)> MapReindeerStats(string input)
+        {
+            Regex pattern = new(@"(.*) can fly (.*) km\/s for (.*) seconds, but then must rest for (.*) seconds.");
+
+            using var reader = new StringReader(input);
+
+            for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
+            {
+                var found = pattern.Match(line);
+
+                if (int.TryParse(found.Groups[2].Value, out int speed) && int.TryParse(found.Groups[3].Value, out int duration) && int.TryParse(found.Groups[4].Value, out int rest))
+                    yield return (found.Groups[1].Value, speed, duration, rest, 0, true);
+            }
         }
     }
 }
