170496ec480dc8fb9730a70dcb095dfe126a1da4
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-25 14:17:25 -0600

    event 2016 unit test project creation

---
+++ src/CodeAdvent.Event.Y2016/Puzzles/Template.cs
@@ -0,0 +1,30 @@
+namespace CodeAdvent.Event.Y2016.Puzzles
+{
+    /// <summary>
+    /// </summary>
+    [Ignore("template unit test does not need to be ran")]
+    public class Template
+    {
+        private string _input;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _input = await CodeAdventData.GetData(0, 0);
+
+            Assert.That(_input, Is.Not.Null.Or.Empty);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.Pass();
+        }
+    }
+}
