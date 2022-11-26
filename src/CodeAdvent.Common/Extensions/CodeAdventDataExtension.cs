ffbaeae2c76eed58cf4f42114f1d244b52d300c6
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-25 21:04:18 -0600

    refactored extension method names

--- src/CodeAdvent.Common/Extensions/CodeAdventDataExtension.cs
+++ src/CodeAdvent.Common/Extensions/CodeAdventDataExtension.cs
@@ -5,7 +5,24 @@ namespace CodeAdvent.Common.Extensions
 {
     public static class CodeAdventDataExtension
     {
-        public static IEnumerable<T> Map<T>(this CodeAdventEvent codeAdventEvent, string pattern, Func<Match, T> action)
+        public static IEnumerable<T> ToEnumerable<T>(this CodeAdventEvent codeAdventEvent)
+        {
+            using var reader = new StringReader(codeAdventEvent.Input);
+
+            for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
+                yield return (T)Convert.ChangeType(line, typeof(T)); ;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this CodeAdventEvent codeAdventEvent, Func<string, T> action)
+        {
+            using var reader = new StringReader(codeAdventEvent.Input);
+
+            for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
+                yield return action(line);
+
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this CodeAdventEvent codeAdventEvent, string pattern, Func<Match, T> action)
         {
             Regex find = new(pattern);
 
@@ -14,14 +31,6 @@ namespace CodeAdvent.Common.Extensions
             for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
                 foreach (Match match in find.Matches(line).Cast<Match>())
                     yield return action(match);
-        }
-
-        public static IEnumerable<string> ToEnumerable(this CodeAdventEvent codeAdventEvent)
-        {
-            using var reader = new StringReader(codeAdventEvent.Input);
-
-            for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
-                yield return line;
         }
     }
 }
