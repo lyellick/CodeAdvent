e7072e0cc5056e58a4e548c87f6e028c925d1db3
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-25 21:32:03 -0600

    added title. puzzle uri, and calendar uri to helper class

--- src/CodeAdvent.Common/CodeAdventData.cs
+++ src/CodeAdvent.Common/CodeAdventData.cs
@@ -1,5 +1,7 @@
 ﻿using RestSharp;
 using CodeAdvent.Common.Models;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace CodeAdvent.Common
 {
@@ -15,23 +17,33 @@ namespace CodeAdvent.Common
         /// <exception cref="UriFormatException"></exception>
         public static async Task<CodeAdventEvent> GetEvent(int year, int day)
         {
+            CodeAdventEvent codeAdventEvent = new();
+
+            Regex pattern = new(@"<h2>--- Day \d: (.*) ---<\/h2>");
+
             if (!TryGetEnvironmentVariable("AOCSession", out string token))
                 throw new Exception("Missing environment variable: AOCSession.");
 
-            if (!Uri.TryCreate($@"https://adventofcode.com/{year}/day/{day}/input", UriKind.RelativeOrAbsolute, out Uri uri))
+            if (!Uri.TryCreate($@"https://adventofcode.com/{year}", UriKind.RelativeOrAbsolute, out Uri calendarUri))
+                throw new UriFormatException();
+
+            if (!Uri.TryCreate($@"https://adventofcode.com/{year}/day/{day}/input", UriKind.RelativeOrAbsolute, out Uri puzzleInputUri))
+                throw new UriFormatException();
+
+            if (!Uri.TryCreate($@"https://adventofcode.com/{year}/day/{day}", UriKind.RelativeOrAbsolute, out Uri puzzlePageUri))
                 throw new UriFormatException();
 
             string cache = Path.Join(Path.GetTempPath(), $"AOC{year}{day:00}.txt");
 
             if (!File.Exists(cache))
             {
-                using var client = new RestClient(uri);
+                using var puzzleInputClient = new RestClient(puzzleInputUri);
 
                 RestRequest request = new();
 
                 request.AddHeader("Cookie", $"session={token}");
 
-                RestResponse response = await client.ExecuteAsync(request);
+                RestResponse response = await puzzleInputClient.ExecuteAsync(request);
 
                 if (response.IsSuccessStatusCode)
                 {
@@ -39,17 +51,29 @@ namespace CodeAdvent.Common
 
                     string input = await File.ReadAllTextAsync(cache);
 
-                    return new() { Year = year, Day = day, Input = input };
+                    codeAdventEvent = new() { Year = year, Day = day, Input = input };
                 }
             }
             else
             {
                 string input = await File.ReadAllTextAsync(cache);
 
-                return new() { Year = year, Day = day, Input = input };
+                codeAdventEvent = new() { Year = year, Day = day, Input = input };
             }
 
-            return new() { Year = year, Day = day, Input = null };
+            using HttpClient puzzlePageClient = new();
+
+            string puzzlePage = await puzzlePageClient.GetStringAsync(puzzlePageUri);
+
+            if (!string.IsNullOrEmpty(puzzlePage))
+            {
+                var match = pattern.Match(puzzlePage);
+                codeAdventEvent.Title = match.Groups[1].Value;
+                codeAdventEvent.Calendar = calendarUri;
+                codeAdventEvent.Puzzle = puzzlePageUri;
+            }
+
+            return codeAdventEvent;
         }
 
         private static bool TryGetEnvironmentVariable(string key, out string value)
--- src/CodeAdvent.Common/Models/CodeAdventEvent.cs
+++ src/CodeAdvent.Common/Models/CodeAdventEvent.cs
@@ -2,10 +2,16 @@
 {
     public class CodeAdventEvent
     {
+        public string Title { get; set; }
+
         public int Year { get; set; }
 
         public int Day { get; set; }
 
         public string Input { get; set; }
+
+        public Uri Calendar { get; set;  }
+
+        public Uri Puzzle { get; set; }
     }
 }
