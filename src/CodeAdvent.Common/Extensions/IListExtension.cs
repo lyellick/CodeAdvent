3a90a21c881239cdfe2df4a66d9288f939afa828
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-25 20:53:38 -0600

    added list split method to IList extension

--- src/CodeAdvent.Common/Extensions/IListExtension.cs
+++ src/CodeAdvent.Common/Extensions/IListExtension.cs
@@ -11,41 +11,56 @@
         public static IEnumerable<IList<T>> GeneratePermutations<T>(this IList<T> source)
         {
             int length = source.Count;
+            var indices = new int[length];
+            var variations = new HashSet<T>();
 
-            var startIndices = new int[length];
-            var variationElements = new HashSet<T>();
-
-            while (startIndices[0] < source.Count)
+            while (indices[0] < source.Count)
             {
                 var variation = new List<T>(length);
                 var valid = true;
+
                 for (int i = 0; i < length; ++i)
                 {
-                    var element = source[startIndices[i]];
-                    if (variationElements.Contains(element))
+                    var element = source[indices[i]];
+                    if (variations.Contains(element))
                     {
                         valid = false;
                         break;
                     }
                     variation.Add(element);
-                    variationElements.Add(element);
+                    variations.Add(element);
                 }
-                if (valid)
-                    yield return variation;
 
-                startIndices[length - 1]++;
+                if (valid)
+                    yield return variation.ToArray();
+
+                indices[length - 1]++;
+
                 for (int i = length - 1; i > 0; --i)
                 {
-                    if (startIndices[i] >= source.Count)
+                    if (indices[i] >= source.Count)
                     {
-                        startIndices[i] = 0;
-                        startIndices[i - 1]++;
+                        indices[i] = 0;
+                        indices[i - 1]++;
                     }
                     else
                         break;
                 }
-                variationElements.Clear();
+
+                variations.Clear();
             }
         }
+
+        /// <summary>
+        /// Generate splits of a given list with each group by bring n size.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static IEnumerable<IList<T>> GenerateSplits<T>(this IList<T> source, int size) => 
+            source.Select((item, index) => new { Index = index, Value = item })
+                  .GroupBy(item => item.Index / size)
+                  .Select(group => group.Select(item => item.Value).ToArray());
     }
 }
