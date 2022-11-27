bd1ea9aa59589f1882c72122c7bb281fe59ffea4
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-27 17:22:49 -0600

    part 1 continued

--- src/CodeAdvent.Event.Y2020/Puzzles/Day04.cs
+++ src/CodeAdvent.Event.Y2020/Puzzles/Day04.cs
@@ -1,3 +1,6 @@
+using CodeAdvent.Common.Extensions;
+using System.Text.RegularExpressions;
+
 namespace CodeAdvent.Event.Y2020.Puzzles
 {
     /// <summary>
@@ -12,12 +15,18 @@ namespace CodeAdvent.Event.Y2020.Puzzles
         {
             _puzzle = await CodeAdventData.GetPuzzle(2020, 4);
 
+            _puzzle.Input = _puzzle.Input.Replace("\n", " ").Replace("  ", "\n");
+
             Assert.That(_puzzle.Input, Is.Not.Null.Or.Empty);
         }
 
         [Test]
         public void Part1()
         {
+            var fields = new Dictionary<string, string> { { "byr", "Birth Year" }, { "iyr", "Issue Year" }, { "eyr", "Expiration Year" }, { "hgt", "Height" }, { "hcl", "Hair Color" }, { "ecl", "Eye Color" }, { "pid", "Passport ID" }, { "cid", "Country ID" } };
+
+            var passports = _puzzle.ToEnumerable(@"(\w+):([^\s]+)", (matches) => matches.ToDictionary(match => match.Groups[1].Value, match => match.Groups[2].Value)).ToArray();
+
             Assert.Pass();
         }
 
