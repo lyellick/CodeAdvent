a572f6265966fc519c104de78945cfe716c8d7ad
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-27 16:29:29 -0600

    day 4 setup

---
+++ src/CodeAdvent.Event.Y2020/Puzzles/Day04.cs
@@ -0,0 +1,30 @@
+namespace CodeAdvent.Event.Y2020.Puzzles
+{
+    /// <summary>
+    /// Day 4: Passport Processing
+    /// </summary>
+    public class Day04
+    {
+        private CodeAdventPuzzle _puzzle;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _puzzle = await CodeAdventData.GetPuzzle(2020, 4);
+
+            Assert.That(_puzzle.Input, Is.Not.Null.Or.Empty);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.Pass();
+        }
+    }
+}
