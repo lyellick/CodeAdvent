1b8b90bfe3e60dbc4600ea229d545c6057b74407
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-26 21:06:07 -0600

    completed day 1

--- src/CodeAdvent.Event.Y2020/Puzzles/Day01.cs
+++ src/CodeAdvent.Event.Y2020/Puzzles/Day01.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace CodeAdvent.Event.Y2020.Puzzles
 {
     /// <summary>
@@ -18,13 +20,56 @@ namespace CodeAdvent.Event.Y2020.Puzzles
         [Test]
         public void Part1()
         {
-            Assert.Pass();
+            int answer = 0, index = 0;
+
+            var report = _puzzle.ToEnumerable<int>().ToArray();
+
+            do
+            {
+                int n1 = report[index];
+                int n2 = 0;
+
+                for (int j = 0; j < report.Length; j++)
+                    if (index != j && n1 + report[j] == 2020)
+                        n2 = report[j];
+
+                if (n1 + n2 == 2020)
+                    answer = n1 * n2;   
+
+                index++;
+            } while (answer == 0);
+
+            Assert.That(answer, Is.EqualTo(181044));
         }
 
         [Test]
         public void Part2()
         {
-            Assert.Pass();
+            int answer = 0, index = 0;
+
+            var report = _puzzle.ToEnumerable<int>().ToArray();
+
+            do
+            {
+                int n1 = report[index];
+                int n2 = 0;
+                int n3 = 0;
+
+                for (int j = 0; j < report.Length; j++)
+                    for (int p = 0; p < report.Length; p++)
+                        if (index != j && index != p && j != p && n1 + report[j] + report[p] == 2020)
+                        {
+                            n2 = report[j];
+                            n3 = report[p];
+                        }
+
+                if (n1 + n2 + n3 == 2020)
+                    answer = n1 * n2 * n3;
+
+                index++;
+            } while (answer == 0);
+
+            Assert.That(answer, Is.EqualTo(82660352));
         }
     }
 }
