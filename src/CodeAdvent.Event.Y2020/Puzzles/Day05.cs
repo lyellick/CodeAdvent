bce37331ba9c41e8b7bdc55c70773e23a212cc57
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-28 15:38:23 -0600

    part 1 checkpoint

--- src/CodeAdvent.Event.Y2020/Puzzles/Day05.cs
+++ src/CodeAdvent.Event.Y2020/Puzzles/Day05.cs
@@ -17,7 +17,20 @@ namespace CodeAdvent.Event.Y2020.Puzzles
         [Test]
         public void Part1()
         {
-            var calls = _puzzle.ToEnumerable((line) => line.Split("")).ToArray();
+            int rows = 128, cols = 8;
+
+            var calls = _puzzle.ToEnumerable((line) => line.ToArray()).ToArray();
+
+            int[][] seats = new int[rows][].Select(i => i = new int[cols]).ToArray();
+
+            for (int row = 0; row < rows; row++)
+                for (int col = 0; col < cols; col++)
+                    seats[row][col] = row;
+
+            foreach (var instructions in calls)
+            {
+                ProcessSeatBSP(seats, instructions, 0);
+            }
 
             Assert.Pass();
         }
@@ -26,6 +39,49 @@ namespace CodeAdvent.Event.Y2020.Puzzles
         public void Part2()
         {
             Assert.Pass();
+        }
+
+        private (int row, int col) ProcessSeatBSP(int[][] seats, char[] instructions, int position)
+        {
+            var seat = (0, 0);
+
+            if (position < instructions.Length)
+            {
+                char instruction = instructions[position];
+
+                if (position < instructions.Length - 3 && seats.Length != 1)
+                {
+                    int center = seats.Length / 2;
+
+                    switch (instruction)
+                    {
+                        case 'F':
+                            seats = seats[0..center];
+                            break;
+                        case 'B':
+                            seats = seats[center..seats.Length];
+                            break;
+                    }
+                }
+                else
+                {
+                    int center = seats[^1].Length / 2;
+
+                    switch (instruction)
+                    {
+                        case 'L':
+                            seats[^1] = seats[^1][0..center];
+                            break;
+                        case 'R':
+                            seats[^1] = seats[^1][center..seats[^1].Length];
+                            break;
+                    }
+                }
+
+                seat = ProcessSeatBSP(seats, instructions, position + 1);
+            }
+
+            return seat;
         }
     }
 }
