36c42f845296178163d3c63934cf7890cd8dc001
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-29 14:42:54 -0600

    completed part 2

--- src/CodeAdvent.Event.Y2020/Puzzles/Day06.cs
+++ src/CodeAdvent.Event.Y2020/Puzzles/Day06.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace CodeAdvent.Event.Y2020.Puzzles
 {
     /// <summary>
@@ -12,16 +14,14 @@ namespace CodeAdvent.Event.Y2020.Puzzles
         {
             _puzzle = await CodeAdventData.GetPuzzle(2020, 6);
 
-            //_puzzle.Input = "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb";
-
-            _puzzle.Input = _puzzle.Input.Replace("\n", " ").Replace("  ", "\n");
-
             Assert.That(_puzzle.Input, Is.Not.Null.Or.Empty);
         }
 
         [Test]
         public void Part1()
         {
+            _puzzle.Input = _puzzle.Input.Replace("\n", " ").Replace("  ", "\n");
+
             var groups = _puzzle.ToEnumerable((line) => line.Split(" ").SelectMany(arry => arry).ToHashSet());
 
             var count = groups.Select(group => group.Count).Sum();
@@ -32,51 +32,12 @@ namespace CodeAdvent.Event.Y2020.Puzzles
         [Test]
         public void Part2()
         {
-            var groups = _puzzle
-                .ToEnumerable((line) => line.Split(" "))
-                .Select(group => group.Select(section => section.ToHashSet().OrderBy(person => person).ToArray()).ToArray())
-                .ToArray();
+            var count = _puzzle.Input.Trim().Split("\n\n")
+                .Select(x => x.Split("\n").Select(l => l.ToCharArray().Distinct()))
+                .Select(g => g.Aggregate((prev, next) => prev.Intersect(next).ToArray()).Count())
+                .Sum();
 
-            List<int> answers = new();
-
-            for (int group = 0; group < groups.Length; group++)
-            {
-                if (groups[group].Length == 1)
-                {
-                    answers.Add(1);
-                }
-                else
-                {
-                    Dictionary<char, int> counts = new();
-
-                    for (int section = 0; section < groups[group].Length; section++)
-                    {
-                        for (int person = 0; person < groups[group][section].Length; person++)
-                        {
-                            char c = groups[group][section][person];
-
-                            bool exists = counts.ContainsKey(c);
-
-                            if (exists)
-                            {
-                                counts[c]++;
-                            }
-                            else
-                            {
-                                counts.Add(c, 1);
-                            }
-                        }
-                    }
-
-                    var answer = counts.Where(kvp => kvp.Value == groups[group].Length).Count();
-
-                    answers.Add(answer);
-                }
-            }
-
-            var count = answers.Sum();
-
-            Assert.Pass();
+            Assert.That(count, Is.EqualTo(3235));
         }
     }
 }
