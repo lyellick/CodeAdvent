a44a75547f080b7f2c06e071a103268acd1758b2
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-29 14:45:24 -0600

    day 7 creation

---
+++ src/CodeAdvent.Event.Y2020/Puzzles/Day07.cs
@@ -0,0 +1,30 @@
+namespace CodeAdvent.Event.Y2020.Puzzles
+{
+    /// <summary>
+    /// Day 7: Handy Haversacks
+    /// </summary>
+    public class Day07
+    {
+        private CodeAdventPuzzle _puzzle;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _puzzle = await CodeAdventData.GetPuzzle(2020, 7);
+
+            Assert.That(_puzzle.Input, Is.Not.Null.Or.Empty);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.Pass();
+        }
+    }
+}
