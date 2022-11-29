7fd3dee7a796460ad145d15093ae552928fd114c
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-11-28 21:46:07 -0600

    checkpoint part 2

--- src/CodeAdvent.Event.Y2020/Puzzles/Day06.cs
+++ src/CodeAdvent.Event.Y2020/Puzzles/Day06.cs
@@ -12,6 +12,8 @@ namespace CodeAdvent.Event.Y2020.Puzzles
         {
             _puzzle = await CodeAdventData.GetPuzzle(2020, 6);
 
+            //_puzzle.Input = "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb";
+
             _puzzle.Input = _puzzle.Input.Replace("\n", " ").Replace("  ", "\n");
 
             Assert.That(_puzzle.Input, Is.Not.Null.Or.Empty);
@@ -30,6 +32,50 @@ namespace CodeAdvent.Event.Y2020.Puzzles
         [Test]
         public void Part2()
         {
+            var groups = _puzzle
+                .ToEnumerable((line) => line.Split(" "))
+                .Select(group => group.Select(section => section.ToHashSet().OrderBy(person => person).ToArray()).ToArray())
+                .ToArray();
+
+            List<int> answers = new();
+
+            for (int group = 0; group < groups.Length; group++)
+            {
+                if (groups[group].Length == 1)
+                {
+                    answers.Add(1);
+                }
+                else
+                {
+                    Dictionary<char, int> counts = new();
+
+                    for (int section = 0; section < groups[group].Length; section++)
+                    {
+                        for (int person = 0; person < groups[group][section].Length; person++)
+                        {
+                            char c = groups[group][section][person];
+
+                            bool exists = counts.ContainsKey(c);
+
+                            if (exists)
+                            {
+                                counts[c]++;
+                            }
+                            else
+                            {
+                                counts.Add(c, 1);
+                            }
+                        }
+                    }
+
+                    var answer = counts.Where(kvp => kvp.Value == groups[group].Length).Count();
+
+                    answers.Add(answer);
+                }
+            }
+
+            var count = answers.Sum();
+
             Assert.Pass();
         }
     }
