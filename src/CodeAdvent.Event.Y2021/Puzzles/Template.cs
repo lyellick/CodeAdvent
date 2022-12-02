9790347a25793d1c029f50700751357e04421db1
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-12-02 11:08:20 -0600

    event 2021 setup

---
+++ src/CodeAdvent.Event.Y2021/Puzzles/Template.cs
@@ -0,0 +1,30 @@
+namespace CodeAdvent.Event.Y2021.Puzzles
+{
+    /// <summary>
+    /// </summary>
+    [Ignore("template unit test does not need to be ran")]
+    public class Template
+    {
+        private CodeAdventPuzzle _puzzle;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _puzzle = await CodeAdventData.GetPuzzle(0, 0);
+
+            Assert.That(_puzzle.Input, Is.Not.Null.Or.Empty);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.Pass();
+        }
+    }
+}
