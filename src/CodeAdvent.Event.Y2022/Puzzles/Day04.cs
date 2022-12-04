69fca77175fa192dea2574ed546122276f65e7ae
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-12-04 13:06:10 -0600

    completed part 1

--- src/CodeAdvent.Event.Y2022/Puzzles/Day04.cs
+++ src/CodeAdvent.Event.Y2022/Puzzles/Day04.cs
@@ -1,6 +1,9 @@
+using CodeAdvent.Common.Extensions;
+
 namespace CodeAdvent.Event.Y2022.Puzzles
 {
     /// <summary>
+    /// Day 4: Camp Cleanup
     /// </summary>
     public class Day04
     {
@@ -17,7 +20,25 @@ namespace CodeAdvent.Event.Y2022.Puzzles
         [Test]
         public void Part1()
         {
-            Assert.Pass();
+            var assignments = _puzzle.ToEnumerable<(int[] first, int[] second)>(@"(\d+)-(\d+),(\d+)-(\d+)", (match) => 
+            {
+                (int start, int end) first = (int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
+                (int start, int end) second = (int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value));
+
+                if (first.start == first.end && second.start != second.end)
+                    return (new int[] { first.start }, Enumerable.Range(second.start, second.end - second.start + 1).ToArray());
+
+                if (first.start != first.end && second.start == second.end)
+                    return (Enumerable.Range(first.start, first.end - first.start + 1).ToArray(), new int[] { second.start });
+
+                return (Enumerable.Range(first.start, first.end - first.start + 1).ToArray(), Enumerable.Range(second.start, second.end - second.start + 1).ToArray());
+            }).ToArray();
+
+            var intersects = assignments.Select(assignment => assignment.first.Intersect(assignment.second).Count() == assignment.first.Length || assignment.first.Intersect(assignment.second).Count() == assignment.second.Length);
+
+            var overlap = intersects.Count(group => group);
+
+            Assert.That(overlap, Is.EqualTo(509));
         }
 
         [Test]
