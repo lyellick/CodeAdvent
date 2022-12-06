b9fd0163f2cdd752f8bb905f9b516400e8fbe92a
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-12-06 15:27:10 -0600

    day 7 setup

---
+++ src/CodeAdvent.Event.Y2022/Puzzles/Day07.cs
@@ -0,0 +1,29 @@
+namespace CodeAdvent.Event.Y2022.Puzzles
+{
+    /// <summary>
+    /// </summary>
+    public class Day07
+    {
+        private CodeAdventPuzzle _puzzle;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _puzzle = await CodeAdventData.GetPuzzle(2022, 7);
+
+            Assert.That(_puzzle.Input, Is.Not.Null.Or.Empty);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.Pass();
+        }
+    }
+}
