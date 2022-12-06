cd0c29d67e4e7d714b3ea5b438bd7d00f2b8e13c
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-12-05 20:58:58 -0600

    added helper extention for jagged arrays.

---
+++ src/CodeAdvent.Common/Extensions/JaggedArrayExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeAdvent.Common.Extensions
+{
+    public static class JaggedArrayExtension
+    {
+        public static List<List<T>> Pivot<T>(this List<List<T>> jarray)
+        {
+            var output = new List<List<T>>();
+            for (int col = 0; col < jarray[0].Count; col++)
+                if (typeof(T) == typeof(string))
+                {
+                    output.Add(Enumerable.Range(0, jarray.Count).Select(i => jarray[i][col]).Where(container => !string.IsNullOrWhiteSpace(container.ToString())).ToList());
+                }
+                else
+                {
+                    output.Add(Enumerable.Range(0, jarray.Count).Select(i => jarray[i][col]).ToList());
+                }
+
+            return output;
+        }
+    }
+}
