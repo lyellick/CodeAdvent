78ae41844382f9b1372e71c22d2bd1c8a5fe51a7
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-12-05 23:14:56 -0600

    day 6 setup

---
+++ src/CodeAdvent.Event.Y2022/Puzzles/Template - Copy.cs
@@ -0,0 +1,30 @@
+namespace CodeAdvent.Event.Y2022.Puzzles
+{
+    /// <summary>
+    /// Day 6: Tuning Trouble
+    /// </summary>
+    public class Day06
+    {
+        private CodeAdventPuzzle _puzzle;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _puzzle = await CodeAdventData.GetPuzzle(2022, 6);
+
+            Assert.That(_puzzle.Input, Is.Not.Null.Or.Empty);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.Pass();
+        }
+    }
+}
