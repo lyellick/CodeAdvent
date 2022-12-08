148fba24ff6bf096c8bde2886785a174333ae967
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-12-07 21:21:06 -0600

    added helper class for day 7

---
+++ src/CodeAdvent.Common/Models/VirtualDirectory.cs
@@ -0,0 +1,25 @@
+﻿namespace CodeAdvent.Common.Models
+{
+    public class VirtualDirectory
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<VirtualDirectory> Directories { get; set; }
+
+        public List<(Guid parent, string name, int size)> Files { get; set; }
+
+        public VirtualDirectory(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Directories = new();
+            Files = new();
+        }
+
+        public void AddDirectory(string name) => Directories.Add(new(name));
+
+        public void AddFile(string name, int size) => Files.Add(new(Id, name, size));
+    }
+}
