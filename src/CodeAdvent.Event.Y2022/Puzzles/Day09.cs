33e8dac3074cad022848550c8051a97a83480931
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-12-09 08:25:31 -0600

    day 9 setup

---
+++ src/CodeAdvent.Event.Y2022/Puzzles/Day09.cs
@@ -0,0 +1,30 @@
+namespace CodeAdvent.Event.Y2022.Puzzles
+{
+    /// <summary>
+    /// Day 9: Rope Bridge
+    /// </summary>
+    public class Day09
+    {
+        private CodeAdventPuzzle _puzzle;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _puzzle = await CodeAdventData.GetPuzzle(2022, 9);
+
+            Assert.That(_puzzle.Input, Is.Not.Null.Or.Empty);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.Pass();
+        }
+    }
+}
