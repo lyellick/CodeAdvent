970c28d01bf945dff343ec1de2c11888fd71eead
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-12-09 08:57:36 -0600

    Created helper method to create jarrays faster.

--- src/CodeAdvent.Event.Y2022/Usings.cs
+++ src/CodeAdvent.Event.Y2022/Usings.cs
@@ -1,4 +1,5 @@
 global using NUnit.Framework;
 global using CodeAdvent.Common;
 global using CodeAdvent.Common.Models;
+global using CodeAdvent.Common.Statics;
 global using CodeAdvent.Common.Extensions;
