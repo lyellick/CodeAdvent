d4c635a5acb198832335008c305a64be831b148f
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-12-13 15:11:05 -0600

    day 13 setup

---
+++ src/CodeAdvent.Event.Y2022/Puzzles/Day13.cs
@@ -0,0 +1,30 @@
+namespace CodeAdvent.Event.Y2022.Puzzles
+{
+    /// <summary>
+    /// Day 13: Distress Signal
+    /// </summary>
+    public class Day13
+    {
+        private CodeAdventPuzzle _puzzle;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _puzzle = await CodeAdventData.GetPuzzle(2022, 13);
+
+            Assert.That(_puzzle.Input, Is.Not.Null.Or.Empty);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.Pass();
+        }
+    }
+}
