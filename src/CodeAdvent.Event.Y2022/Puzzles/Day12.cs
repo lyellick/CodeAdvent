b8a0695ae87f885582db7c0355e46fb7868b0cd9
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-12-13 14:49:48 -0600

    I DID IT

--- src/CodeAdvent.Event.Y2022/Puzzles/Day12.cs
+++ src/CodeAdvent.Event.Y2022/Puzzles/Day12.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+
 namespace CodeAdvent.Event.Y2022.Puzzles
 {
     /// <summary>
@@ -20,25 +22,38 @@ namespace CodeAdvent.Event.Y2022.Puzzles
         [Test]
         public void Part1()
         {
-            int steps = 0;
-
-            (int row, int col) s = (20, 0), e = (20, 88);
+            (int row, int col) begining = (20, 0), end = (20, 88);
 
             var map = _puzzle
                 .ToEnumerable((line) => line.Select(c => c - 97))
-                .Select((cols, row) => cols.Select((elevation, col) => new Cell(row, col, elevation, e)).ToArray())
+                .Select((cols, row) => cols.Select((elevation, col) => new Cell(row, col, elevation, end)).ToArray())
                 .ToArray();
 
-            Cell start = map[s.row][s.col], finish = map[e.row][e.col], current = start;
+            Cell start = map[begining.row][begining.col], finish = map[end.row][end.col];
 
-            List<Cell> path = new() { start }, queue = new();
+            start.Parent = 0;
+
+            Queue<Cell> queue = new(new[] { start });
+
+            int steps = 0;
 
             do
             {
+                var current = queue.Dequeue();
+
                 var neighbors = current.GetNeighbors(map);
 
-                path.Add(neighbors.First());
-            } while (start != finish);
+                var available = neighbors.Where(cell => cell.Parent == null).Where(cell => cell.Elevation <= current.Elevation + 1).ToArray();
+
+                if (current.Id == finish.Id)
+                    steps = current.Parent.Value;
+
+                foreach (var cell in available)
+                {
+                    cell.Parent = current.Parent + 1;
+                    queue.Enqueue(cell);
+                }
+            } while (steps == 0);
 
             Assert.Pass();
         }
@@ -52,6 +67,10 @@ namespace CodeAdvent.Event.Y2022.Puzzles
 
     public class Cell
     {
+        public Guid Id { get; set; }
+
+        public int? Parent { get; set; }
+
         public int Row { get; set; }
 
         public int Col { get; set; }
@@ -60,8 +79,10 @@ namespace CodeAdvent.Event.Y2022.Puzzles
 
         public double Distance { get; set; }
 
-        public Cell(int row, int col, int elevation, (int row, int col) end)
+        public Cell(int row, int col, int elevation, (int row, int col) end, int? seen = null)
         {
+            Id = Guid.NewGuid();
+            Parent = seen;
             Row = row;
             Col = col;
             Elevation = elevation;
@@ -77,7 +98,6 @@ namespace CodeAdvent.Event.Y2022.Puzzles
             neighbors = locations
                 .Where(location => map.IsWithinBounds(location.row, location.col))
                 .Select(location => map[location.row][location.col])
-                .OrderBy(cell => cell.Distance).ThenBy(cell => cell.Elevation)
                 .ToArray();
 
             return neighbors;
