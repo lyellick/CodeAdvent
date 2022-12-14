cd97f1f3490f9a8e3fcabd2f0226109568f51e27
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-12-14 11:11:52 -0600

    day 14 setup

---
+++ src/CodeAdvent.Event.Y2022/Puzzles/Day14.cs
@@ -0,0 +1,30 @@
+namespace CodeAdvent.Event.Y2022.Puzzles
+{
+    /// <summary>
+    /// Day 14: Regolith Reservoir
+    /// </summary>
+    public class Day14
+    {
+        private CodeAdventPuzzle _puzzle;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _puzzle = await CodeAdventData.GetPuzzle(2022, 14);
+
+            Assert.That(_puzzle.Input, Is.Not.Null.Or.Empty);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.Pass();
+        }
+    }
+}
