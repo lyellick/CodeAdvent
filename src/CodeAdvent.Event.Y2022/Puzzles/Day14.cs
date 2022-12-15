eeb221ba434072f6086b86a6423c32595d9158f9
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-12-14 21:40:58 -0600

    i will make a partical simulator... just not tonight.

--- src/CodeAdvent.Event.Y2022/Puzzles/Day14.cs
+++ src/CodeAdvent.Event.Y2022/Puzzles/Day14.cs
@@ -1,6 +1,8 @@
 ﻿using CodeAdvent.Common.Extensions;
 using CodeAdvent.Common.Models;
 using System.Drawing;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
@@ -33,7 +35,14 @@ namespace CodeAdvent.Event.Y2022.Puzzles
 
             var simultion = new CaveSimultion(layout, (0, 500), (158, 506));
 
-            string cave = simultion.ToString();
+            bool drop = true;
+
+            do
+            {
+                simultion.ProcessGrain();
+
+                string cave = simultion.ToString();
+            } while (drop);
 
             Assert.Pass();
         }
@@ -65,11 +74,17 @@ namespace CodeAdvent.Event.Y2022.Puzzles
             _cave[start.row][start.col] = 3;
         }
 
+        public void ProcessGrain()
+        {
+            Grain grain = new(_start);
+
+        }
+
         public override string ToString()
         {
             string output = "";
 
-            Dictionary<int, string> map = new() 
+            Dictionary<int, string> map = new()
             {
                 { 0, "."},
                 { 1, "#"},
@@ -80,7 +95,7 @@ namespace CodeAdvent.Event.Y2022.Puzzles
             for (int row = 0; row < _cave.Length; row++)
             {
                 string line = "";
-                
+
                 for (int col = 0; col < _cave[row].Length; col++)
                     line += map[_cave[row][col]];
 
@@ -89,5 +104,26 @@ namespace CodeAdvent.Event.Y2022.Puzzles
 
             return output;
         }
+    }
+
+    public class Grain
+    {
+        public int Row { get; set; }
+
+        public int Col { get; set; }
+
+        public virtual Grain Previous { get; set; }
+
+        public Grain((int row, int col) start, Grain previous = null)
+        {
+            Row = start.row;
+            Col = start.col;
+            Previous = previous;
+        }
+    }
+
+    public static class CaveExtension
+    {
+        public static int GetValue(this int[][] cave, Grain sand) => cave[sand.Row][sand.Col];
     }
 }
