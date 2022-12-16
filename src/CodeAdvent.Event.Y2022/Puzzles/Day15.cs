a73a17de45c4dac95a6afabbe84ffbafd7dc9bdb
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-12-15 21:27:44 -0600

    F'K YOU PART 1

--- src/CodeAdvent.Event.Y2022/Puzzles/Day15.cs
+++ src/CodeAdvent.Event.Y2022/Puzzles/Day15.cs
@@ -20,15 +20,40 @@ namespace CodeAdvent.Event.Y2022.Puzzles
         [Test]
         public void Part1()
         {
+            var row = 2000000;
+
+            var cols = new HashSet<int>();
+
             string pattern = @"Sensor at x=(.+), y=(.+): closest beacon is at x=(.+), y=(.+)";
 
             var scan = _puzzle.ToEnumerable<((int row, int col) sensor, (int row, int col) beacon)>(pattern, (line) => 
             {
                 var groups = line.Groups.ToEnumerable<int>().ToArray();
+
                 return ((groups[1], groups[0]), (groups[3], groups[2]));
             }).ToArray();
 
-            Assert.Pass();
+            var exlude = scan.Select(relation => relation.beacon).Distinct().Count(beacon => beacon.row == row);
+
+            for (int i = 0; i < scan.Length; i++)
+            {
+                (int row, int col) sensor = scan[i].sensor, beacon = scan[i].beacon;
+
+                var distance = sensor.ManhattanDistanceTo(beacon) - Math.Abs(row - sensor.row);
+
+                var overlap = (distance * 2) + 1;
+
+                if (overlap <= 0)
+                    continue;
+                    
+                var start = sensor.col - distance;
+
+                Array.ForEach(Enumerable.Range(Convert.ToInt32(start), Convert.ToInt32(overlap)).ToArray(), col => { cols.Add(col); });
+            }
+
+            var none = cols.Count - exlude;
+
+            Assert.That(none, Is.EqualTo(5838453));
         }
 
         [Test]
