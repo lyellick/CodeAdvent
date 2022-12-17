44069110fd166df12378dc5369ef9de82948803f
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-12-16 20:52:57 -0600

    day 16 setup

--- src/CodeAdvent.Event.Y2022/Puzzles/Day16.cs
+++ src/CodeAdvent.Event.Y2022/Puzzles/Day16.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json.Linq;
+
 namespace CodeAdvent.Event.Y2022.Puzzles
 {
     /// <summary>
@@ -18,6 +20,20 @@ namespace CodeAdvent.Event.Y2022.Puzzles
         [Test]
         public void Part1()
         {
+            string pattern = @"Valve (.+) has flow rate=(.+); tunnels? leads? to valves? (.+)";
+
+            var report = _puzzle
+                .ToEnumerable<(string label, string rate, string[] joins)>(pattern, (valve) =>
+                    (valve.Groups[1].Value, valve.Groups[2].Value, valve.Groups[3].Value.Split(", ")))
+                .ToArray();
+
+            Valve[] valves = report
+                .Select(valve => new Valve(valve.label, valve.rate))
+                .OrderBy(valve => valve.Label)
+                .ToArray();
+
+            Array.ForEach(valves, (valve) => valve.MapConnections(report, valves));
+
             Assert.Pass();
         }
 
@@ -25,6 +41,31 @@ namespace CodeAdvent.Event.Y2022.Puzzles
         public void Part2()
         {
             Assert.Pass();
+        }
+    }
+
+    public class Valve
+    {
+        public string Label { get; set; }
+
+        public int Rate { get; set; }
+
+        public virtual Valve[] Connections { get; set; }
+
+        public Valve(string label, string rate)
+        {
+            Label = label;
+            Rate = int.Parse(rate);
+        }
+
+        public void MapConnections((string label, string rate, string[] joins)[] report, Valve[] valves)
+        {
+            var joins = report.First(valve => valve.label == Label).joins;
+
+            Connections = valves
+                .Where(valve => joins.Contains(valve.Label))
+                .OrderBy(valve => valve.Label)
+                .ToArray();
         }
     }
 }
